12d5a298cc6a4fd651b70171397416c04d861220
Kaitrin Marinaro <kait.can.create@Kaitrins-MBP.localdomain>
2019-11-05 19:36:04 -0500

    Updated Scoreboard Wk 2 Day 2

--- public/main.js
+++ public/main.js
@@ -17,11 +17,19 @@ const teamTwoUpdate = () => {
 // change team one score up
 
 const teamOneAddOne = () => {
-  document.querySelector('.team-1-score').textContent =
-    parseInt(document.querySelector('.team-1-score').textContent) + 1
+  if (document.querySelector('.team-1-score').textContent <= 20) {
+    document.querySelector('.team-1-score').textContent =
+      parseInt(document.querySelector('.team-1-score').textContent) + 1
+  }
+  if (document.querySelector('.team-1-score').textContent == 21) {
+    disableButtons()
+    displayWinner()
+    document.querySelector('.reset').style.display = 'block'
+    document.querySelector('.reset-button').disabled = false
+  }
 }
 
-// change team one score down
+// change team one score down, not to go below zero
 
 const teamOneSubtractOne = () => {
   if (document.querySelector('.team-1-score').textContent >= 1) {
@@ -33,11 +41,19 @@ const teamOneSubtractOne = () => {
 // change team two score up
 
 const teamTwoAddOne = () => {
-  document.querySelector('.team-2-score').textContent =
-    parseInt(document.querySelector('.team-2-score').textContent) + 1
+  if (document.querySelector('.team-2-score').textContent <= 20) {
+    document.querySelector('.team-2-score').textContent =
+      parseInt(document.querySelector('.team-2-score').textContent) + 1
+  }
+  if (document.querySelector('.team-2-score').textContent == 21) {
+    disableButtons()
+    displayWinner()
+    document.querySelector('.reset').style.display = 'block'
+    document.querySelector('.reset-button').disabled = false
+  }
 }
 
-// change team two score down
+// Change team two score down, not to go below zero
 
 const teamTwoSubtractOne = () => {
   if (document.querySelector('.team-2-score').textContent >= 1) {
@@ -46,6 +62,77 @@ const teamTwoSubtractOne = () => {
   }
 }
 
+// When a team gets 21 points, disable the buttons
+
+const disableButtons = () => {
+  for (let i = 0; i < document.querySelectorAll('button').length; i++) {
+    document.querySelectorAll('button')[i].disabled = true
+  }
+}
+
+// When a team gets to 21 points, display a message declaring the winning team
+
+const displayWinner = () => {
+  console.log(
+    document.querySelector('.team-1-name').textContent +
+      ' vs ' +
+      document.querySelector('.team-2-name').textContent +
+      ' = ' +
+      document.querySelector('.team-1-score').textContent +
+      ':' +
+      document.querySelector('.team-2-score').textContent
+  )
+  if (
+    parseInt(document.querySelector('.team-1-score').textContent) >
+    parseInt(document.querySelector('.team-2-score').textContent)
+  ) {
+    document.querySelector('.winning-team').textContent =
+      document.querySelector('.team-1-name').textContent + ' Wins!'
+  }
+  if (
+    parseInt(document.querySelector('.team-2-score').textContent) >
+    parseInt(document.querySelector('.team-1-score').textContent)
+  ) {
+    document.querySelector('.winning-team').textContent =
+      document.querySelector('.team-2-name').textContent + ' Wins!'
+  }
+  if (
+    document.querySelector('.team-1-score').textContent ==
+    document.querySelector('.team-2-score').textContent
+  ) {
+    document.querySelector('.winning-team').textContent = 'Tie!'
+  }
+}
+
+// + Resets score to 0
+// + Clears winner message
+// + Clear Team names
+// + Re-enable buttons
+// - Reset button would disappear
+
+const resetScoreboard = () => {
+  document.querySelector('.team-1-score').textContent = '0'
+  document.querySelector('.team-2-score').textContent = '0'
+  document.querySelector('.winning-team').textContent = ''
+  document.querySelector('.team-1-name').textContent = 'Team 1'
+  document.querySelector('.team-2-name').textContent = 'Team 2'
+  for (let i = 0; i < document.querySelectorAll('button').length; i++) {
+    document.querySelectorAll('button')[i].disabled = false
+  }
+  for (let i = 0; i < document.querySelectorAll('input').length; i++) {
+    document.querySelectorAll('input')[i].value = ''
+  }
+  document.querySelector('.reset').style.display = 'none'
+
+  // Notes for me: above is the explanation for the expression above. i starts off as 0, 7 buttons, looking to see each time does the instance (class or tag) reappear based on is it less than 7. i++ means to add one in JS
+  // document.querySelectorAll('button')[0].disabled = false
+  // document.querySelectorAll('button')[1].disabled = false
+  // document.querySelectorAll('button')[2].disabled = false
+  // document.querySelectorAll('button')[3].disabled = false
+  // document.querySelectorAll('button')[4].disabled = false
+  // document.querySelectorAll('button')[5].disabled = false
+  // document.querySelectorAll('button')[6].disabled = false
+}
 // Updates team one name only after clicking update button- event listener
 
 document
@@ -70,14 +157,20 @@ document
   .querySelector('.team-1-subtract-1-button')
   .addEventListener('click', teamOneSubtractOne)
 
-// Adds one point to team two's score: event listener waits and updates only after clicking right add1 button
+// Adds one point to team two's score, up to 21: event listener waits and updates only after clicking right add1 button
 
 document
   .querySelector('.team-2-add-1-button')
   .addEventListener('click', teamTwoAddOne)
 
-// Adds one point to team two's score, so long as the score is greater than 0: event listener waits and updates only after clicking right subtract1 button
+// Adds one point to team two's score, up to 21: so long as the score is greater than 0: event listener waits and updates only after clicking right subtract1 button
 
 document
   .querySelector('.team-2-subtract-1-button')
   .addEventListener('click', teamTwoSubtractOne)
+
+// Resets the team score to zero
+
+document
+  .querySelector('.reset-button')
+  .addEventListener('click', resetScoreboard)
